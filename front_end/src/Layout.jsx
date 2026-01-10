f82321a345ffa2c6d43cb3de4279cb7034be1cfe
SELLAHEWA T.N. <tharushasellahewa00@gmail.com>
2026-01-11 01:04:29 +0530

    fied all errors

--- front_end/src/Layout.jsx
+++ front_end/src/Layout.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useLocation } from 'react-router-dom';
 import Navigation from "./assets/common/Navigation";
 import Footer from "./assets/common/Footer";
 import WhatsAppPopup from "./assets/common/WhatsAppPopup";
 
 function Layout({children}) {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
       <Navigation />
