285a9974fd287e6ed27eca8a0e2a882f86825d31
Wenusha Rashindi <127926916+nydwenusha@users.noreply.github.com>
2026-08-26 15:26:20 +0530

    Update cors.php

--- api/config/cors.php
+++ api/config/cors.php
@@ -20,6 +20,8 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
+        'https://cey-tripz-frontend.onrender.com',
+        'https://cey-tripz-admin.onrender.com',
         'http://localhost:5173',
         'http://localhost:5174',
         'http://127.0.0.1:5173',  // ← Add this
