a7fbd787ec63a3ff22d8e358bae9bc1cad6cff6c
Wenusha Rashindi <127926916+nydwenusha@users.noreply.github.com>
2026-09-09 21:27:36 +0530

    Update AuthState.jsx

--- admin_panel/src/services/auth/AuthState.jsx
+++ admin_panel/src/services/auth/AuthState.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import api from './api/api';
+import api from '../api/api';
 
 // Create the context
 export const AuthContext = createContext();
